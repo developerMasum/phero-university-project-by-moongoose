d11c6e71a0e93c5b8e6e3eca52708a93d9c15ac4
developerMasum <developer.masum2001@gmail.com>
2023-12-02 23:25:25 +0600

    academic faculty added

---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+import { academicFacultyValidation } from './academicFaculty.validation';
+
+
+
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    academicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/:facultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
+
+router.patch(
+  '/:facultyId',
+  validateRequest(
+    academicFacultyValidation.updateAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+export const AcademicFacultyRoutes = router;
